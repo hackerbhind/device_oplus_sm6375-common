7294f9b3430d755134a6ca5e9669fdad54d63849
hackerbhind <144337511+hackerbhind@users.noreply.github.com>
2023-10-29 05:33:56 +0530

    Correct files name typo error
    framework_compatibility_matrix.xml

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -87,7 +87,7 @@ LOC_HIDL_VERSION := 4.0
 # HIDL
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
 
 # Kernel
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
