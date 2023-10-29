ee46b5bd85f8d012aae54314acd1b0686a5205f9
hackerbhind <144337511+hackerbhind@users.noreply.github.com>
2023-10-29 07:01:25 +0530

    Solve separater error
    Line 117

--- common.mk
+++ common.mk
@@ -113,8 +113,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     vendor.qti.hardware.display.allocator-service \
-    vendor.qti.hardware.display.compo
-ser-service \
+    vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.mapper@1.0.vendor \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
